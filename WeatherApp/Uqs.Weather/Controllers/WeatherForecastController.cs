85911554d553f03422829452eccb044aec13d646
Piotr Mulinski <piotr.mulinski@icloud.com>
2026-01-06 18:06:20 +0100

    first commit

---
+++ WeatherApp/Uqs.Weather/Controllers/WeatherForecastController.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Mvc;
+using AdamTibi.OpenWeather;
+
+namespace Uqs.Weather.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private const int FORECAST_DAYS = 7;
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IConfiguration _config;
+
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild",
+        "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _config = config;
+    }
+
+    [HttpGet("ConvertCToF")]
+    public double ConvertCToF(double c)
+    {
+        double f = c * (9d / 5d) + 32;
+        _logger.LogInformation("conversion requested");
+        return f;
+    }
+
+    [HttpGet("GetRealWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>> GetReal()
+    {
+        decimal ANTWERPEN_LAT = 51.2194m;
+        decimal ANTWERPEN_LON = 4.4025m;
+
+        string apiKey = _config["OpenWeather:Key"];
+
+        HttpClient httpClient = new HttpClient();
+
+        OneCall30Client openWeatherClient = new OneCall30Client(apiKey, httpClient);
+
+        OneCallResponse res = await openWeatherClient.OneCallAsync
+            (ANTWERPEN_LAT, ANTWERPEN_LON, new [] {
+                Excludes.Current, Excludes.Minutely,
+                Excludes.Hourly, Excludes.Alerts }, Units.Metric);
+
+        WeatherForecast[] wfs = new WeatherForecast[FORECAST_DAYS];
+        for (int i = 0; i < wfs.Length; i++)
+        {
+            var wf = wfs[i] = new WeatherForecast();
+            wf.Date = res.Daily[i + 1].Dt;
+            double forecastedTemp = res.Daily[i + 1].Temp.Day;
+            wf.TemperatureC = (int)Math.Round(forecastedTemp);
+            wf.Summary = MapFeelToTemp(wf.TemperatureC);
+        }
+        return wfs;
+    }
+
+    [HttpGet("GetRandomWeatherForecast")]
+    public IEnumerable<WeatherForecast> GetRandom()
+    {
+        WeatherForecast[] wfs = new WeatherForecast[FORECAST_DAYS];
+        for(int i = 0;i < wfs.Length;i++)
+        {
+            var wf = wfs[i] = new WeatherForecast();
+            wf.Date = DateTime.Now.AddDays(i + 1);
+            wf.TemperatureC = Random.Shared.Next(-20, 55);
+            wf.Summary = MapFeelToTemp(wf.TemperatureC);
+        }
+        return wfs;
+    }
+
+    private static string MapFeelToTemp(int temperatureC)
+    {
+        // Anything <= 0 is "Freezing"
+        if (temperatureC <= 0)
+        {
+            return Summaries.First();
+        }
+        // Dividing the temperature into 5 intervals
+        int summariesIndex = (temperatureC / 5) + 1;
+        // Anything >= 45 is "Scorching"
+        if (summariesIndex >= Summaries.Length)
+        {
+            return Summaries.Last();
+        }
+        return Summaries[summariesIndex];
+    }
+}
---
+++ WeatherApp/Uqs.Weather/OneCall30Client.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace AdamTibi.OpenWeather;
+
+public class OneCall30Client : IClient
+{
+    private readonly string _apiKey;
+
+    private readonly HttpClient _httpClient;
+
+    private const string BASE_URL = "https://api.openweathermap.org/data/3.0";
+
+    public OneCall30Client (string apiKey, HttpClient httpClient)
+    {
+        _apiKey = apiKey;
+        _httpClient = httpClient;
+    }
+
+    public async Task<OneCallResponse> OneCallAsync(decimal latitude, decimal longitude, IEnumerable<Excludes> excludes, Units unit)
+    {
+        UriBuilder uriBuilder = new UriBuilder("https://api.openweathermap.org/data/3.0/onecall");
+        NameValueCollection nameValueCollection = HttpUtility.ParseQueryString("");
+        nameValueCollection["lat"] = latitude.ToString();
+        nameValueCollection["lon"] = longitude.ToString();
+        nameValueCollection["appid"] = _apiKey;
+        if (excludes != null && excludes.Any())
+        {
+            nameValueCollection["exclude"] = string.Join(',', excludes).ToLower();
+        }
+
+        nameValueCollection["units"] = unit.ToString().ToLower();
+        uriBuilder.Query = nameValueCollection.ToString();
+        string value = await _httpClient.GetStringAsync(uriBuilder.Uri.AbsoluteUri);
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new InvalidOperationException("No response from the service");
+        }
+
+        return JsonConvert.DeserializeObject<OneCallResponse>(value) ?? throw new Exception();
+    }
+}
+#if false // Decompilation log
+'307' items in cache
+------------------
+Resolve: 'System.Runtime, Version=6.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+Found single assembly: 'System.Runtime, Version=8.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+WARN: Version mismatch. Expected: '6.0.0.0', Got: '8.0.0.0'
+Load from: 'C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.22\ref\net8.0\System.Runtime.dll'
+------------------
+Resolve: 'System.Net.Http, Version=6.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+Found single assembly: 'System.Net.Http, Version=8.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+WARN: Version mismatch. Expected: '6.0.0.0', Got: '8.0.0.0'
+Load from: 'C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.22\ref\net8.0\System.Net.Http.dll'
+------------------
+Resolve: 'System.Collections.Specialized, Version=6.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+Found single assembly: 'System.Collections.Specialized, Version=8.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+WARN: Version mismatch. Expected: '6.0.0.0', Got: '8.0.0.0'
+Load from: 'C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.22\ref\net8.0\System.Collections.Specialized.dll'
+------------------
+Resolve: 'System.ComponentModel.Annotations, Version=6.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+Found single assembly: 'System.ComponentModel.Annotations, Version=8.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+WARN: Version mismatch. Expected: '6.0.0.0', Got: '8.0.0.0'
+Load from: 'C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.22\ref\net8.0\System.ComponentModel.Annotations.dll'
+------------------
+Resolve: 'Newtonsoft.Json, Version=13.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed'
+Found single assembly: 'Newtonsoft.Json, Version=13.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed'
+Load from: 'C:\Users\piotr\.nuget\packages\newtonsoft.json\13.0.1\lib\netstandard2.0\Newtonsoft.Json.dll'
+------------------
+Resolve: 'System.Collections, Version=6.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+Found single assembly: 'System.Collections, Version=8.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+WARN: Version mismatch. Expected: '6.0.0.0', Got: '8.0.0.0'
+Load from: 'C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.22\ref\net8.0\System.Collections.dll'
+------------------
+Resolve: 'System.Web.HttpUtility, Version=6.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'
+Found single assembly: 'System.Web.HttpUtility, Version=8.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'
+WARN: Version mismatch. Expected: '6.0.0.0', Got: '8.0.0.0'
+Load from: 'C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.22\ref\net8.0\System.Web.HttpUtility.dll'
+------------------
+Resolve: 'System.Linq, Version=6.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+Found single assembly: 'System.Linq, Version=8.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+WARN: Version mismatch. Expected: '6.0.0.0', Got: '8.0.0.0'
+Load from: 'C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.22\ref\net8.0\System.Linq.dll'
+------------------
+Resolve: 'System.Runtime.InteropServices, Version=6.0.0.0, Culture=neutral, PublicKeyToken=null'
+Found single assembly: 'System.Runtime.InteropServices, Version=8.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+WARN: Version mismatch. Expected: '6.0.0.0', Got: '8.0.0.0'
+Load from: 'C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.22\ref\net8.0\System.Runtime.InteropServices.dll'
+------------------
+Resolve: 'System.Runtime.CompilerServices.Unsafe, Version=6.0.0.0, Culture=neutral, PublicKeyToken=null'
+Found single assembly: 'System.Runtime.CompilerServices.Unsafe, Version=8.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+WARN: Version mismatch. Expected: '6.0.0.0', Got: '8.0.0.0'
+Load from: 'C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.22\ref\net8.0\System.Runtime.CompilerServices.Unsafe.dll'
+------------------
+Resolve: 'System.Runtime, Version=8.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+Found single assembly: 'System.Runtime, Version=8.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+Load from: 'C:\Program Files\dotnet\packs\Microsoft.NETCore.App.Ref\8.0.22\ref\net8.0\System.Runtime.dll'
+#endif
